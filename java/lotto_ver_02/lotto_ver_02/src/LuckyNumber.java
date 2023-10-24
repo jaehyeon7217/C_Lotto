76086601eee1bb93df09240392ced8eede4f41ba
jaehyeon park <jhp1276@naver.com>
2023-10-24 15:48:42 +0900

    java lotto, creation number

--- java/lotto_ver_02/lotto_ver_02/src/LuckyNumber.java
+++ java/lotto_ver_02/lotto_ver_02/src/LuckyNumber.java
@@ -7,46 +7,118 @@ public class LuckyNumber {
 
     private boolean[] check;
 
-    public LuckyNumber(){
-        num = new int[6];
-        sum = 0;
-        sumMin = 21;
-        sumMax = 255;
-        check = new boolean[45];
+//    public LuckyNumber(){
+//        boolean[] check = new boolean[45];
+//        new LuckyNumber(21,255,check);
+//    }
+//
+//    public LuckyNumber(int sumMin, int sumMax){
+//        boolean[] check = new boolean[45];
+//        new LuckyNumber(sumMin,sumMax,check);
+//    }
+//
+//    public LuckyNumber(boolean[] check){
+//        new LuckyNumber(21,255,check);
+//    }
+
+    public LuckyNumber(int sumMin, int sumMax, boolean[] check){
+        this.num = new int[6];
+        this.sum = 0;
+        this.sumMin = sumMin;
+        this.sumMax = sumMax;
+        this.check = check;
     }
 
-    public void setSumRange(int min, int max){
-        if(min<21 || min >=255) {
-            System.out.println("최소 범위는 21이상 255미만이어야 합니다.");
-        }
-        if(max<=21 || max>255){
-            System.out.println("최소 범위는 21초과 255이하이어야 합니다.");
-        }
-        this.sumMin=min;
-        this.sumMax=max;
-    }
-
-    public void resetSumRange(){
-        this.sumMax=21;
-        this.sumMin=255;
-    }
-
-    public void setExceptNum(int[] exNum){
-        for(int i=0;i<exNum.length;i++){
-            if(exNum[i]<=0 || exNum[i]>45){
-                System.out.println("범위를 초과한 수가 들어왔습니다.");
-                resetExceptNum();
-                return;
+    public void createNumber(){
+        while(true){
+            int[] tempNum = getNumber();
+            int sum = 0;
+            for(int i=0;i<6;i++){
+                sum += tempNum[i];
             }
-            check[exNum[i]]=true;
+            if(sum>=this.sumMin && sum<=this.sumMax){
+                this.num = tempNum;
+                this.sum = sum;
+                break;
+            }
         }
+
+        pr();
+
     }
 
-    public void resetExceptNum(){
-        // 초기화
-        for(int j=0;j<45;j++){
-            check[j]=false;
+    public void pr(){
+        for(int i=0;i<6;i++){
+            System.out.print(this.num[i] + " ");
         }
+        System.out.print("   " + this.sum);
+        System.out.println();
     }
+
+    public int[] getNumber(){
+        boolean[] deduplication = new boolean[45];
+
+        for(int i=0;i<45;i++){
+            if(check[i]){
+                deduplication[i] = true;
+            }
+        }
+
+        boolean[] lotto = new boolean[45];
+        int count = 0;
+
+        while(count<6){
+            int num = (int)(Math.random()*45);
+            if(deduplication[num]) continue;
+            deduplication[num] = true;
+            lotto[num] = true;
+            count++;
+        }
+
+        int[] lottoNum = new int[6];
+        count = 0;
+        for(int i=0;i<45;i++){
+            if(lotto[i]){
+                lottoNum[count++] = i+1;
+                if(count == 6) break;
+            }
+        }
+
+        return lottoNum;
+    }
+
+//    public void setSumRange(int min, int max){
+//        if(min<21 || min >=255) {
+//            System.out.println("최소 범위는 21이상 255미만이어야 합니다.");
+//        }
+//        if(max<=21 || max>255){
+//            System.out.println("최소 범위는 21초과 255이하이어야 합니다.");
+//        }
+//        this.sumMin=min;
+//        this.sumMax=max;
+//    }
+//
+//    public void resetSumRange(){
+//        this.sumMax=21;
+//        this.sumMin=255;
+//    }
+//
+//    public void setExceptNum(int[] exNum){
+//        for(int i=0;i<exNum.length;i++){
+//            if(exNum[i]<=0 || exNum[i]>45){
+//                System.out.println("범위를 초과한 수가 들어왔습니다.");
+//                resetExceptNum();
+//                return;
+//            }
+//            check[exNum[i]]=true;
+//        }
+//    }
+//
+//    public void resetExceptNum(){
+//        // 초기화
+//        for(int j=0;j<45;j++){
+//            check[j]=false;
+//        }
+//    }
 
 }
--- java/lotto_ver_02/lotto_ver_02/src/Main.java
+++ java/lotto_ver_02/lotto_ver_02/src/Main.java
@@ -3,38 +3,47 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        /*
+        입력받기 위한 BufferedReader 선언
+        입력받은 문자열을 자르기위한 StringTokenizer 선언
+         */
+
         StringTokenizer st;
 
+        /*
+        1. 금주 회차 입력
+        thisWeekNo => 이번 주 회차 (가장 최신 회차)
+         */
         int thisWeekNo = 0;
 
-        while(true) {
+        /*
+        입력 맞을 때 까지 입력 및 검증 반복
+         */
+        while (true) {
             System.out.println("금주 회자를 입력해주세요. (예) 1086");
-            String tempStr = br.readLine();
-            while(!numberOnlyCheck(tempStr)){
-                System.out.println("숫자가 아닌 다른 문자가 포함되었습니다. 다시 입력해 주세요.");
-                tempStr = br.readLine();
-            }
-            while(!numberRangeCheck(tempStr)){
-                System.out.println("10억이 넘는 수입니다. 다시 입력해 주세요.");
-                tempStr = br.readLine();
-            }
+            String tempStr = inputStr(1,999999999);
+
             thisWeekNo = Integer.parseInt(tempStr);
             System.out.println("입력하신 회차는 " + thisWeekNo + "회 입니다. 맞으면 1번 틀리면 2번을 눌러주세요.");
-            int tempFlag = Integer.parseInt(br.readLine());
-            while(!(tempFlag == 1 || tempFlag == 2)){
-                System.out.println("잘 못 입력하셨습니다. 다시 입력해 주세요.");
-                tempFlag = Integer.parseInt(br.readLine());
-            }
-            if(tempFlag == 1) break;
-            else{
+            tempStr = inputStr(1,2);
+
+            int tempFlag = Integer.parseInt(tempStr);
+            if (tempFlag == 1) break;
+            else {
                 System.out.println("재입력을 선택하셨습니다.");
             }
         }
         System.out.println("금주의 회차는 " + thisWeekNo + "회 입니다.");
 
         makeLine();//////////////////////////////////////////////////////////////////////////
+
+        /*
+        2. 역대 당첨번호 입력
+         */
 
         System.out.println("역대 당첨번호를 입력합니다.");
         System.out.println("동행 복권사이트에서 다운받은 엑셀을 (1회차부터 입력회차 까지) 긁어 넣습니다. 이때 회차는 역순 상태 그대로 입력합니다.");
@@ -43,21 +52,19 @@ public class Main {
 
         // 이 부분에 번호 제대로 들어갔는지 검증하는 방법
 
-
-
-        for(int i=thisWeekNo-1; i>=0;i--){
-            st = new StringTokenizer(br.readLine(),"\t");
+        for (int i = thisWeekNo - 1; i >= 0; i--) {
+            st = new StringTokenizer(br.readLine(), "\t");
             int sumTemp = 0;
-            for(int j=0;j<6;j++){
-                if(!st.hasMoreTokens()) {
+            for (int j = 0; j < 6; j++) {
+                if (!st.hasMoreTokens()) {
                     System.out.println("6자리 이하의 로또번호가 들어왔습니다. ");
                 }
                 String check = st.nextToken();
-                if(!numberOnlyCheck(check)){
-                    System.out.println("숫자가 아닙니다. 혹은 \\t로 개행하신 것 맞나요?");
+                if (!numberOnlyCheck(check)) {
+                    System.out.println("숫자가 아닙니다. 혹은 \\t로 개행하신 것 맞나요? (엑셀을 그대로 복사할 시 \\t로 개행 됨)");
                 }
                 historyNum[i][j] = Integer.parseInt(check);
-                if(historyNum[i][j] <= 0 || historyNum[i][j] >45){
+                if (historyNum[i][j] <= 0 || historyNum[i][j] > 45) {
                     System.out.println("범위가 0이하거나 45를 초과했습니다.");
                 }
                 sumTemp += historyNum[i][j];
@@ -73,36 +80,184 @@ public class Main {
 //            System.out.println(historyNum[i][6]);
 //        }
 
+        /*
+        3. 동작 선택
+            1. 번호 생성
+            2. 통계 조회
+            3. 경우의 수 조회
+            4. 종료
+         */
 
+        boolean operator = true;
+        while(operator){
+            System.out.println("동작을 선택해주세요.");
+            System.out.println("1. 번호 생성");
+            System.out.println("2. 번호 통계 조회");
+            System.out.println("3. 경우의 수 조회");
+            System.out.println("0. 종료");
+
+            int op = Integer.parseInt(inputStr(0, 3));
+
+
+            if(op == 0){
+                operator = false;
+                break;
+            } else if(op == 1){
+                System.out.println("- 번호를 생성합니다.");
+                System.out.println("생성할 조합의 수를 정하세요. 최소 1, 최대 100");
+                int lottoCount = Integer.parseInt(inputStr(1, 100));
+                System.out.println(lottoCount + "개의 번호 조합을 생성합니다.");
+
+                int sumMin = 21;
+                int sumMax = 255;
+
+
+                System.out.println("구간합을 설정하시려면 1번, 전체 구간에서 번호 출력을 원하시면 0번을 입력해주세요.");
+                int tempNum = Integer.parseInt(inputStr(0,1));
+
+                boolean sumFlag = true;
+
+                if(tempNum == 0){
+                    System.out.println("다음 단계로 넘어갑니다.");
+                    sumFlag = false;
+                }
+
+
+                while(sumFlag){
+                    System.out.println("번호 합 구간을 설정합니다. 생성된 번호의 합은 입력한 최솟값과 최댓값의 사이에서 생성됩니다.");
+                    System.out.println("** ex) 120 ~ 150으로 설정되었다면 번호합이 119이하나 151이상은 버리고 새로운 조합을 생성합니다.");
+                    System.out.println("최소 값을 설정하세요. 최소 21, 최대 254입니다.");
+                    int tempMin = Integer.parseInt(inputStr(21,254));
+                    System.out.println("최대 값을  설정하세요. 최소 22, 최대 255입니다.");
+                    int tempMax = Integer.parseInt(inputStr(22, 255));
+                    if(tempMin >= tempMax){
+                        System.out.println("최솟값이 최댓값보다 크거나 같습니다. 다시 입력합니다.");
+                    }else {
+                        System.out.println("입력하신 최소 값은 "+ tempMin + "최대 값은 " + tempMax + "입니다.");
+                        System.out.println("맞으면 1번 틀리면 2번을 입력해주세요.");
+                        tempNum = Integer.parseInt(inputStr(1,2));
+                        if(tempNum == 1){
+                            System.out.println("설정이 완료되었습니다.");
+                            sumMin = tempMin;
+                            sumMax = tempMax;
+                            sumFlag = false;
+                        }else{
+                            System.out.println("설정을 다시 시작합니다.");
+                        }
+                    }
+                }
+
+                System.out.println("미사용 번호를 입력합니다.");
+                System.out.println("입력을 원하시면 1번 아니면 0번을 눌러주세요.");
+                tempNum = Integer.parseInt(inputStr(0,1));
+
+                boolean notUseNumberFlag = true;
+
+                if(tempNum == 0){
+                    System.out.println("다음 단계로 넘어갑니다.");
+                    notUseNumberFlag = false;
+                }
+
+                boolean[] notUseNumberCheck = new boolean[45];
+
+                System.out.println("사용하지 않을 번호를 입력하세요. 다시 입력하면 사용하기로 바뀝니다.");
+                System.out.println("입력이 끝나면 0번을 입력해주세요.");
+                while(notUseNumberFlag){
+                    int tempNumber = Integer.parseInt(inputStr(0, 45));
+                    if(tempNumber == 0){
+                        System.out.println("입력을 종료합니다.");
+                        printNotUseNumber(notUseNumberCheck);
+                        notUseNumberFlag=false;
+                        break;
+                    }
+                    tempNumber -= 1;
+                    notUseNumberCheck[tempNumber] = !notUseNumberCheck[tempNumber];
+                    if(notUseNumberCheck[tempNumber]){
+                        System.out.println((tempNumber+1) + "번을 사용하지 않습니다.");
+                    }else{
+                        System.out.println((tempNumber+1) + "번을 사용합니다.");
+                    }
+                }
+
+                System.out.println("번호 생성을 시작합니다.");
+                LuckyNumber[] lotto = new LuckyNumber[lottoCount];
+                for(int i=0;i<lottoCount;i++){
+                    lotto[i] = new LuckyNumber(sumMin, sumMax, notUseNumberCheck);
+                    lotto[i].createNumber();
+                }
+
+                br.readLine();
+
+            } else if(op == 2){
+                System.out.println(" 번호 통계를 조회합니다.");
+            } else if(op == 3){
+                System.out.println(" 경우의 수를 조회합니다.");
+            }
+        }
 
     }
 
-    public static void cleanScreen(){
-        for(int i=0;i<80;i++){
+    public static void printNotUseNumber(boolean[] check){
+        System.out.println("제외할 번호를 출력합니다.");
+        for(int i=0;i<45;i++){
+            if(check[i]){
+                System.out.print( (i+1) + "번 ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static String inputStr(int one, int two) throws Exception{
+        String tempStr = br.readLine();
+        boolean numberOnlyCheckFlag = true;  // 숫자만 있는지 확인
+        boolean numberRangeCheckFlag = true; // 숫자가 10억을 넘어가지 않는지 확인
+        boolean oneOrTwoFlag = true; // 숫자가 one 이상 two 이하 인지 확인
+        while (numberOnlyCheckFlag || numberRangeCheckFlag ||oneOrTwoFlag) {
+            if (numberOnlyCheckFlag = !numberOnlyCheck(tempStr)) {
+                System.out.println("숫자가 아닌 다른 문자가 포함되었습니다. 다시 입력해 주세요.");
+                tempStr = br.readLine();
+            } else if (numberRangeCheckFlag = !numberRangeCheck(tempStr)) {
+                System.out.println("10억이 넘는 수입니다. 다시 입력해 주세요.");
+                tempStr = br.readLine();
+            } else if(oneOrTwoFlag = !oneOrTwoCheck(tempStr, one, two)){
+                System.out.println(one + " 이상 " + two + "이하가 아닌 다른 수를 입력하셨습니다. 다시 입력해 주세요.");
+                tempStr = br.readLine();
+            }
+        }
+        return tempStr;
+    }
+
+    public static void cleanScreen() {
+        for (int i = 0; i < 80; i++) {
             System.out.println();
         }
     }
 
-    public static void makeLine(){
-        for(int i=0;i<40;i++){
+    public static void makeLine() {
+        for (int i = 0; i < 40; i++) {
             System.out.print("-");
         }
         System.out.println();
     }
 
-    public static boolean numberRangeCheck(String num){
-        if(num.length()>=10){
+    public static boolean numberRangeCheck(String num) {
+        if (num.length() >= 10) {
             return false;
         }
         return true;
     }
 
-    public static boolean numberOnlyCheck(String num){
+    public static boolean numberOnlyCheck(String num) {
         final String REGEX = "[0-9]+"; // 숫자 확인 정규 표현식
-        if(num.matches(REGEX)){
+        if (num.matches(REGEX)) {
             return true;
         }
         return false;
+    }
+
+    public static boolean oneOrTwoCheck(String num, int one, int two){
+        int number = Integer.parseInt(num);
+        return number >= one && number <=two;
     }
 
 }
